4acda583b93c57d1c52d9557b05b02466650773f
Brody Wilton <brodywilton@hotmail.com>
2025-01-23 22:18:57 +0000

    Start creating controller, add a few routes

--- src/main/java/com/personal/pantry/menu/web/Controller.java
+++ src/main/java/com/personal/pantry/menu/web/Controller.java
@@ -1,4 +1,30 @@
 package com.personal.pantry.menu.web;
 
+import com.personal.pantry.menu.model.menu.Menu;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    /**
+     * Create access to the repository within the controller
+     *
+     * TODO: Implement the registry class in a separate package
+     * */
+    private MenuRegistry reggie;
+
+    @GetMapping("/")
+    public ResponseEntity<String> index() {
+        //TODO: implement this
+        return ResponseEntity.ok("Welcome to Pantry Menu Application");
+    }
+
+    @GetMapping("/menu")
+    public ResponseEntity<Menu> getMenu() {
+        //  TODO: implement this
+        return null;
+    }
+
 }
